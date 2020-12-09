294bed310d43b94138b944c5abd1f750de3c08e9
unknown <lamjavadev456@gmail.com>
2020-12-09 15:35:31 +0700

    init project

---
+++ database/create_database.sql
@@ -0,0 +1,7 @@
+CREATE USER phamconglam WITH PASSWORD '123456';
+CREATE DATABASE phamconglam OWNER phamconglam ENCODING = 'UTF8';
+CREATE SCHEMA phamconglam;
+
+--create database on server
+--CREATE DATABASE phamconglam OWNER phamconglam ENCODING = 'UTF8' lc_collate='en_US.utf8' lc_ctype='en_US.utf8' template template0;
+
